4b93a50cce08188ad13b237f8db9ba34292aa397
URLeisure <URLeisure@163.com>
2023-05-08 08:49:48 +0800

    1

--- src/tenthlesson/aopanno/UserPoxy.java
+++ src/tenthlesson/aopanno/UserPoxy.java
@@ -1,7 +1,7 @@
 package tenthlesson.aopanno;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,5 +16,26 @@ public class UserPoxy {
     public void before(){
         System.out.println("before in UserPoxy.");
     }
+    @After(value = "execution(* tenthlesson.aopanno.User.add(..))")
+    public void after(){
+        System.out.println("after in UserPoxy.");
+    }
 
+    //异常通知
+    @AfterThrowing(value = "execution(* tenthlesson.aopanno.User.add(..))")
+    public void afterThrowing(){
+        System.out.println("afterThrowing in UserPoxy");
+    }
+
+    @AfterReturning(value = "execution(* tenthlesson.aopanno.User.add(..))")
+    public void afterReturning(){
+        System.out.println("afterReturning in UserPoxy");
+    }
+
+    @Around(value = "execution(* tenthlesson.aopanno.User.add(..))")
+    public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("环绕前");
+        proceedingJoinPoint.proceed();
+        System.out.println("环绕后");
+    }
 }
